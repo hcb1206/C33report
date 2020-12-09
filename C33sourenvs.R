f12d6dcdce51a1f2c2585a808079a81a76d6bb21
hcb1206 <hcb1206@gmail.com>
2020-12-09 13:55:07 -0500

    updating data and script

--- C33sourenvs.R
+++ C33sourenvs.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(psych)
 library(ggplot2)
 library(readxl)
+library(tibble)
 
 setwd("~/R/github/C33report/data")
 df <- read_xlsx("C33environmentals.xlsx", sheet = "environmentals")
@@ -12,4 +13,24 @@ colnames(df)
 df <- pivot_longer(df, 4:11)
 df <- filter(df, !is.na(df$value))
 colnames(df) <- c("date", "pheno", "zone", "metric", "value")
+
 View(df)
+summary(df)
+
+dfpheno <- filter(df, !is.na(df$pheno))
+dfphenoAL <- filter(dfpheno, dfpheno$zone == "AL")
+dfphenoCU <- filter(dfpheno, dfpheno$zone == "CU")
+df <- filter(df, is.na(pheno))
+dfAL <- filter(df, df$zone == "AL")
+dfCU <- filter(df, df$zone == "CU")
+
+times <- unique(df$date)
+
+sourmoisture <- ggplot() + 
+  geom_line(data = filter(dfphenoAL, dfphenoAL$metric == "Moisture Content" & dfphenoAL$pheno == "SOUR011"), 
+            aes(x = date, y = value), color = "blue") +
+  geom_line(data = filter(dfphenoCU, dfphenoCU$metric == "Moisture Content"& dfphenoCU$pheno == "SOUR011"),
+            aes(x = date, y = value), color = "red") +
+  xlab('dates') +
+  ylab('Moisture Content (%)')
+sourmoisture
